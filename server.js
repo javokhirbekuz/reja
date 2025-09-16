2ee821a11e65e366e6c3db7ceb75acfbc9976893
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-09-17 01:32:53 +0900

    fix: deleted unnecessary code in server.js

--- server.js
+++ server.js
@@ -10,23 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 // 2: Session code
 
 // 3: Views code
-// app.set("views", "views");
-// app.set("view engine", "ejs");
-
-// 4: Routing code
-app.get("/hello", (req, res) => {
-    res.send(
-        `<h1 style="color: aqua; text-align: center;  font-size: 128px; ">Hello world</h1>`
-    );
-});
-
-app.get("/gift", (req, res) => {
-    res.send(
-        `<h1 style="color: aqua; text-align: center; font-size: 128px; ">You're at Gifts Page</h1>`
-    );
-});
-
-// const server = http.createServer(app);
 app.set("views", "views");
 app.set("view engine", "ejs");
 
@@ -40,9 +23,6 @@ app.get("/", (req, res) => {
     res.render("harid");
 });
 
-app.get("/hello", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
-});
 const server = http.createServer(app);
 
 const PORT = 3000;
