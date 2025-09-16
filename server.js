b108ed6c72f2a597ee6f06a9aec02a6967ffa9ba
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-09-16 14:14:40 +0900

    fix: modified server.js

--- server.js
+++ server.js
@@ -3,20 +3,13 @@ const app = express();
 const http = require("http");
 
 // // 1: Kirish code
-<<<<<<< HEAD
-// app.use(express.static("public"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-=======
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
->>>>>>> 206253e (BRR: initial setup)
 
 // 2: Session code
 
 // 3: Views code
-<<<<<<< HEAD
 // app.set("views", "views");
 // app.set("view engine", "ejs");
 
@@ -34,7 +27,6 @@ app.get("/gift", (req, res) => {
 });
 
 // const server = http.createServer(app);
-=======
 app.set("views", "views");
 app.set("view engine", "ejs");
 
@@ -52,7 +44,7 @@ app.get("/hello", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
 const server = http.createServer(app);
->>>>>>> 206253e (BRR: initial setup)
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`The app is running on the port: ${PORT}`);
