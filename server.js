ec1852740b6c7dafc29dbd058f622cc6478cc8ec
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-09-19 01:08:50 +0900

    feat: added author route page

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/users.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERRROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 // // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -17,6 +26,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
 });
 
 app.get("/", (req, res) => {
