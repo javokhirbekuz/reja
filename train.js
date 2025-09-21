cc7b0b92a6380e5051e2e4e440ac2347d5b3c4b3
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-09-21 21:21:12 +0900

    feat: solved MITask

--- train.js
+++ train.js
@@ -1,81 +1,107 @@
-// console.log("train task ishga tushdi");
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yahshi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq hato qiling", // 20-30
-    "uzingizga ishlashingizni boshlang", // 30-40
-    "siz kuchli bolgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling, foydasi yoq endi", // 60
-];
+// // console.log("train task ishga tushdi");
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yahshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi", // 60
+// ];
 
-// callback functionlarda setInterval ishlaydi
-function maslahatBering(a, callback) {
-    if (typeof a !== "number") callback("insert a number", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a < 40) callback(null, list[2]);
-    else if (a > 40 && a < 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setTimeout(function () {
-            callback(null, list[5]);
-        }, 1000);
-        // setInterval(function () {
-        //     callback(null, list[5]);
-        // }, 1000);
-    }
-}
-
-console.log("passed here 0");
-maslahatBering(65, (err, data) => {
-    if (err) console.log("ERROR:", err);
-    else {
-        console.log("javob:", data);
-    }
-});
-console.log("passed here 1");
-
-// // Async function
-// async function maslahatBering(a, callback) {
-//     if (typeof a !== "number") throw new Error("Insert an only number!");
-//     else if (a <= 20) return list[0];
-//     else if (a > 20 && a <= 30) return list[1];
-//     else if (a > 30 && a <= 40) return list[2];
-//     else if (a > 40 && a <= 50) return list[3];
-//     else if (a > 50 && a <= 60) return list[4];
+// // callback functionlarda setInterval ishlaydi
+// function maslahatBering(a, callback) {
+//     if (typeof a !== "number") callback("insert a number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a < 40) callback(null, list[2]);
+//     else if (a > 40 && a < 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
 //     else {
-//         return new Promise((resolve, reject) => {
-//             setTimeout(function () {
-//                 // ---> setInterval async functionda va Promise functionda ishlamaydi
-//                 resolve(list[5]);
-//             }, 1000);
-//         });
-//         // return list[5];
-//         // setTimeout(function () {
+//         setTimeout(function () {
+//             callback(null, list[5]);
+//         }, 1000);
+//         // setInterval(function () {
 //         //     callback(null, list[5]);
-//         // }, 5000);
+//         // }, 1000);
 //     }
 // }
-// // call async func vis then/catch
+
 // console.log("passed here 0");
-// maslahatBering(65)
-//     .then((data) => {
-//         console.log("Data:", data);
-//     })
-//     .catch((err) => {
-//         console.log("Error:", err);
-//     });
+// maslahatBering(65, (err, data) => {
+//     if (err) console.log("ERROR:", err);
+//     else {
+//         console.log("javob:", data);
+//     }
+// });
 // console.log("passed here 1");
 
-// // call async func async/await
-// async function run() {
-//     let javob = await maslahatBering(50);
-//     console.log(javob);
-//     javob = await maslahatBering(30);
-//     console.log(javob);
-//     javob = await maslahatBering(42);
-//     console.log(javob);
-// }
+// // // Async function
+// // async function maslahatBering(a, callback) {
+// //     if (typeof a !== "number") throw new Error("Insert an only number!");
+// //     else if (a <= 20) return list[0];
+// //     else if (a > 20 && a <= 30) return list[1];
+// //     else if (a > 30 && a <= 40) return list[2];
+// //     else if (a > 40 && a <= 50) return list[3];
+// //     else if (a > 50 && a <= 60) return list[4];
+// //     else {
+// //         return new Promise((resolve, reject) => {
+// //             setTimeout(function () {
+// //                 // ---> setInterval async functionda va Promise functionda ishlamaydi
+// //                 resolve(list[5]);
+// //             }, 1000);
+// //         });
+// //         // return list[5];
+// //         // setTimeout(function () {
+// //         //     callback(null, list[5]);
+// //         // }, 5000);
+// //     }
+// // }
+// // // call async func vis then/catch
+// // console.log("passed here 0");
+// // maslahatBering(65)
+// //     .then((data) => {
+// //         console.log("Data:", data);
+// //     })
+// //     .catch((err) => {
+// //         console.log("Error:", err);
+// //     });
+// // console.log("passed here 1");
 
-// run();
+// // // call async func async/await
+// // async function run() {
+// //     let javob = await maslahatBering(50);
+// //     console.log(javob);
+// //     javob = await maslahatBering(30);
+// //     console.log(javob);
+// //     javob = await maslahatBering(42);
+// //     console.log(javob);
+// // }
+
+// // run();
+
+const countLetter = (letter, word) => {
+    let count = 0;
+    for (i in word.split("")) {
+        if (word[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+};
+
+const countLetter2 = (letter, word) => {
+    const count = word.split("").reduce((total, curIn) => {
+        if (curIn === letter) {
+            total++;
+        }
+        return total;
+    }, 0);
+    return count;
+};
+console.log(
+    countLetter2(
+        "a",
+        "salom dunyo men sizlarga salom berish uchun Amerikadan etib keldim."
+    )
+);
